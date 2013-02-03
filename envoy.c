ac632510c3bbb15a517c674e855fac3ecc213f9f
Simon Gomizelj <simongmzlj@gmail.com>
2013-02-03 16:04:25 -0500

    envoy: got the first run logic wrong

--- envoy.c
+++ envoy.c
@@ -330,7 +330,7 @@ int main(int argc, char *argv[])
         break;
     case ACTION_ADD:
         /* when there are no agumert, with gpg-agent it should be a no op */
-        if (!data.status == ENVOY_STARTED || data.gpg[0])
+        if (data.status == ENVOY_RUNNING || data.gpg[0])
             return 0;
     case ACTION_FORCE_ADD:
         add_keys(&argv[optind], argc - optind);
