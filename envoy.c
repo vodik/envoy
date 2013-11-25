4c36676b6881133a7c30604e0f6589ea5bb9020a
Simon Gomizelj <simongmzlj@gmail.com>
2013-11-24 23:12:32 -0500

    tweak csh output

--- envoy.c
+++ envoy.c
@@ -149,10 +149,10 @@ static void print_sh_env(struct agent_data_t *data)
 static void print_csh_env(struct agent_data_t *data)
 {
     if (data->type == AGENT_GPG_AGENT)
-        printf("setenv GPG_AGENT_INFO %s;\n", data->gpg);
+        printf("setenv GPG_AGENT_INFO '%s';\n", data->gpg);
 
-    printf("setenv SSH_AUTH_SOCK %s;\n", data->sock);
-    printf("setenv SSH_AGENT_PID %d;\n", data->pid);
+    printf("setenv SSH_AUTH_SOCK '%s';\n", data->sock);
+    printf("setenv SSH_AGENT_PID '%d';\n", data->pid);
 }
 
 static void print_fish_env(struct agent_data_t *data)
