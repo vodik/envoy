cf277f399d4985e20c3f4c978b70beb99e33918b
Simon Gomizelj <simongmzlj@gmail.com>
2014-03-12 18:00:04 -0400

    cleaned up .ycm_extra_conf.py
    Now hits pkg-config when compiling together the flags array

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,16 +1,16 @@
 import os
 import ycm_core
+import subprocess
 from clang_helpers import PrepareClangFlags
 
-# Set this to the absolute path to the folder (NOT the file!) containing the
-# compile_commands.json file to use that instead of 'flags'. See here for
-# more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
-# Most projects will NOT need to set this to anything; you can just change the
-# 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = ''
+database = None
 
-# These are the compilation flags that will be used in case there's no
-# compilation database set.
+def pkg_config(pkg):
+    def not_whitespace(string):
+        return not (string == '' or string == '\n')
+    output = subprocess.check_output(['pkg-config', '--cflags', pkg]).strip()
+    return filter(not_whitespace, output.split(' '))
+
 flags = [
   '-Wall',
   '-Wextra',
@@ -20,18 +20,14 @@ flags = [
   '-DENVOY_VERSION="ycm"',
   '-D_GNU_SOURCE',
   '-std=c99',
-  '-x', 'c',
+  '-x', 'c'
 ]
 
-if compilation_database_folder:
-  database = ycm_core.CompilationDatabase( compilation_database_folder )
-else:
-  database = None
-
+flags += pkg_config('dbus-1')
+flags += pkg_config('libsystemd-daemon')
 
 def DirectoryOfThisScript():
-  return os.path.dirname( os.path.abspath( __file__ ) )
-
+  return os.path.dirname(os.path.abspath( __file__ ))
 
 def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
   if not working_directory:
@@ -44,37 +40,35 @@ def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
 
     if make_next_absolute:
       make_next_absolute = False
-      if not flag.startswith( '/' ):
-        new_flag = os.path.join( working_directory, flag )
+      if not flag.startswith('/'):
+        new_flag = os.path.join(working_directory, flag)
 
     for path_flag in path_flags:
       if flag == path_flag:
         make_next_absolute = True
         break
 
-      if flag.startswith( path_flag ):
-        path = flag[ len( path_flag ): ]
-        new_flag = path_flag + os.path.join( working_directory, path )
+      if flag.startswith(path_flag):
+        path = flag[len(path_flag):]
+        new_flag = path_flag + os.path.join(working_directory, path)
         break
 
     if new_flag:
-      new_flags.append( new_flag )
+      new_flags.append(new_flag)
   return new_flags
 
-
-def FlagsForFile( filename ):
+def FlagsForFile(filename):
   if database:
     # Bear in mind that compilation_info.compiler_flags_ does NOT return a
     # python list, but a "list-like" StringVec object
-    compilation_info = database.GetCompilationInfoForFile( filename )
+    compilation_info = database.GetCompilationInfoForFile(filename)
     final_flags = PrepareClangFlags(
-        MakeRelativePathsInFlagsAbsolute(
-            compilation_info.compiler_flags_,
-            compilation_info.compiler_working_dir_ ),
-        filename )
+        MakeRelativePathsInFlagsAbsolute(compilation_info.compiler_flags_,
+                                         compilation_info.compiler_working_dir_),
+        filename)
   else:
     relative_to = DirectoryOfThisScript()
-    final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
+    final_flags = MakeRelativePathsInFlagsAbsolute(flags, relative_to)
 
   return {
     'flags': final_flags,
