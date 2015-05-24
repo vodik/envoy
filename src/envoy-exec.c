6550f2cad13e896e475255cdcaa61fdde4401465
Simon Gomizelj <simongmzlj@gmail.com>
2015-05-24 14:36:37 -0400

    Drop the symlink support for a better shebang based mechanism

--- src/envoy-exec.c
+++ src/envoy-exec.c
@@ -23,13 +23,14 @@
 #include <err.h>
 #include <errno.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
 #include "agents.h"
 #include "socket.h"
 #include "gpg-protocol.h"
 #include "util.h"
-
-static const char *exe_path;
 
 static void source_agent_env(enum agent id)
 {
@@ -59,45 +60,58 @@ static void source_agent_env(enum agent id)
     putenvf("SSH_AUTH_SOCK=%s", data.sock);
 }
 
-static inline int safe_execv(const char *path, char *const argv[])
+static char *extract_binary(char *path)
 {
-    _cleanup_free_ char *real = realpath(path, NULL);
+    struct stat st;
+    char *memblock;
 
-    if (real && streq(real, exe_path))
-        return 0;
+    _cleanup_close_ int fd = open(path, O_RDONLY);
+    if (fd < 0)
+        return path;
 
-    return execv(path, argv);
+    fstat(fd, &st);
+    memblock = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED | MAP_POPULATE, fd, 0);
+    madvise(memblock, st.st_size, MADV_WILLNEED | MADV_SEQUENTIAL);
+
+    if (memblock[0] != '#' || memblock[1] != '!')
+        return path;
+
+    size_t scan = strcspn(memblock, "\n");
+    scan += strspn(memblock + scan, "\n");
+    size_t eol = strcspn(memblock + scan, "\n");
+    return strndup(memblock + scan, eol);
 }
 
-static _noreturn_ void exec_wrapper(const char *cmd, int argc, char *argv[])
+static _noreturn_ void exec_from_path(char *argv[])
 {
-    /* command + NULL + argv */
-    char *args[argc + 1];
-    int i;
-
-    for (i = 0; i < argc; i++)
-        args[i] = argv[i];
-    args[argc] = NULL;
-
-    if (cmd[0] == '/') {
-        safe_execv(args[0], args);
-        // If the exec failed, the wrapper was called by its full path
-        cmd = program_invocation_short_name;
-    }
-
-    const char *path = getenv("PATH");
+    char *path = strdup(getenv("PATH"));
     if (!path)
-        errx(EXIT_FAILURE, "command %s not found", cmd);
-    _cleanup_free_ char *buf = strdup(path);
+        errx(EXIT_FAILURE, "command %s not found", argv[0]);
 
-    char *saveptr = NULL, *segment = strtok_r(buf, ":", &saveptr);
+    char *saveptr = NULL, *segment = strtok_r(path, ":", &saveptr);
     for (; segment; segment = strtok_r(NULL, ":", &saveptr)) {
-        char *full_path = joinpath(segment, cmd, NULL);
-        safe_execv(full_path, args);
+        char *full_path = joinpath(segment, argv[0], NULL);
+        execv(full_path, argv);
         free(full_path);
     }
 
-    errx(EXIT_FAILURE, "command %s not found", cmd);
+    errx(EXIT_FAILURE, "command %s not found", argv[0]);
+}
+
+static _noreturn_ void exec_wrapper(int argc, char *argv[])
+{
+    /* command + NULL + argv */
+    char *new_argv[argc + 1];
+    char *cmd = extract_binary(argv[0]);
+    int i;
+
+    new_argv[0] = cmd;
+    for (i = 1; i < argc; i++)
+        new_argv[i] = argv[i];
+    new_argv[argc] = NULL;
+
+    execv(cmd, new_argv);
+    exec_from_path(new_argv);
 }
 
 static _noreturn_ void usage(FILE *out)
@@ -114,7 +128,6 @@ static _noreturn_ void usage(FILE *out)
 int main(int argc, char *argv[])
 {
     enum agent type = AGENT_DEFAULT;
-    const char *path = argv[0];
 
     static const struct option opts[] = {
         { "help",    no_argument,       0, 'h' },
@@ -123,43 +136,36 @@ int main(int argc, char *argv[])
         { 0, 0, 0, 0 }
     };
 
-    exe_path = realpath("/proc/self/exe", NULL);
-    if (!exe_path)
-        err(EXIT_FAILURE, "failed to resolve /proc/self/exe");
+    while (true) {
+        int opt = getopt_long(argc, argv, "+hvt:", opts, NULL);
+        if (opt == -1)
+            break;
 
-    if (streq(program_invocation_short_name, "envoy-exec")) {
-        while (true) {
-            int opt = getopt_long(argc, argv, "+hvt:", opts, NULL);
-            if (opt == -1)
-                break;
-
-            switch (opt) {
-            case 'h':
-                usage(stdout);
-                break;
-            case 'v':
-                printf("%s %s\n", program_invocation_short_name, ENVOY_VERSION);
-                return 0;
-            case 't':
-                type = lookup_agent(optarg);
-                if (type < 0)
-                    errx(EXIT_FAILURE, "unknown agent: %s", optarg);
-                break;
-            default:
-                usage(stderr);
-            }
-        }
-
-        path = argv[optind];
-        argc -= optind;
-        argv += optind;
-
-        if (argc == 0)
+        switch (opt) {
+        case 'h':
+            usage(stdout);
+            break;
+        case 'v':
+            printf("%s %s\n", program_invocation_short_name, ENVOY_VERSION);
+            return 0;
+        case 't':
+            type = lookup_agent(optarg);
+            if (type < 0)
+                errx(EXIT_FAILURE, "unknown agent: %s", optarg);
+            break;
+        default:
             usage(stderr);
+        }
     }
 
+    argc -= optind;
+    argv += optind;
+
+    if (argc == 0)
+        usage(stderr);
+
     source_agent_env(type);
-    exec_wrapper(path, argc, argv);
+    exec_wrapper(argc, argv);
 }
 
 // vim: et:sts=4:sw=4:cino=(0
