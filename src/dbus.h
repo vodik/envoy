0002fe0948ff3bda79c46d19b2e5bdbe2bbd5591
Simon Gomizelj <sgomizelj@sangoma.com>
2015-06-28 15:27:46 -0400

    Initial port of envoy to sd-bus

--- src/dbus.h
+++ src/dbus.h
@@ -17,12 +17,11 @@
 
 #pragma once
 
-#include <dbus/dbus.h>
+#include <systemd/sd-bus.h>
 
-DBusConnection *get_connection(DBusBusType type);
-
-int start_transient_unit(DBusConnection *conn, const char *name,
-                         const char *slice, const char *desc, char **ret);
-int get_unit(DBusConnection *conn, const char *name, char **ret);
-int stop_unit(DBusConnection *conn, const char *path, char **ret);
-int get_unit_state(DBusConnection *conn, const char *path, char **ret);
+void start_transient_unit(sd_bus *bus, const char *name,
+                          const char *slice, const char *desc);
+char *get_unit(sd_bus *bus, const char *name);
+void stop_unit(sd_bus *bus, const char *path);
+char *get_unit_state(sd_bus *bus, const char *path);
+sd_bus *get_connection(uid_t uid);
